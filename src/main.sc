61535929edd08e781954e2441bcfeba3bedb6800
d.zakirov@doctis.ru <d.zakirov@doctis.ru>
2022-12-12 09:14:09 +0000

    Edited files:     /src/main.sc     /src/requests.js

--- src/main.sc
+++ src/main.sc
@@ -1,16 +1,20 @@
 require: slotfilling/slotFilling.sc
   module = sys.zb-common
+require: requests.js 
 theme: /
 
     state: Start
         q!: $regex</start>
         a: Привет, я электронный помощник. Скажи погода, чтобы узнать погоду в твоем городе. Если хочешь узнать твое текущее время скажи время
     state: Weather
-        intent!: /погода,
+        intent: /погода
         a: Назови город в котором ты находишься
     state: GetCity
         intent: /город || fromState = "/Weather", onlyThisState = true
-        a: Ищу город
+        go!: /GetWeather
+    state: /GetWeather
+        script: 
+            findWeatherData();
     state: NoMatch || noContext = true
         event!: noMatch
         a: Я тебя не понял, пожалуйста, повтори фразу
---
+++ src/requests.js
@@ -0,0 +1,115 @@
+function findWeatherData() {
+    try {
+        getWeather();
+    } catch(err) {
+        sendTextResponse("Не удалось найти погоду по вашему городу");
+        log(err.message);
+    }
+}
+
+function getWeather() {
+    var url = "api.openweathermap.org/data/2.5/forecast";
+    
+    var result = getPosition();
+    var lat = result.lat;
+    var lon = result.lon;
+    
+    var params = {
+        lat: lat,
+        lon: lon,
+        appid: getAppId(),
+        units: "metric",
+    }
+    
+    var weatherResponse = loadWeatherData(url, params);
+    
+    var state = weatherResponse.data.list[0].weahter[0].main;
+    
+    switch(state) {
+        case "Clear":
+            state = 'Чистое небо';
+            break;
+        case "Snow":
+            state = "Снежно";
+            break;
+        case "Windy": 
+            state = "Ветренно";
+            break;
+        case "Clouds":
+            state = "Облачно";
+            break;
+        case "Thunderstorm":
+            state = "Гроза";
+            break;
+        case "Rain":
+            state = "Дождь";
+            break;
+        case "Clear":
+            state = 'Чисто';
+            break;
+    }
+    
+    var temp = weatherRespose.data.list[0].main.temp;
+    var text = "У вас сейчас: " + state + "\n Температура: " + temp;
+    sendTextResponse(text);
+}
+
+function sendTextResponse(text) {
+    var $response = $context.response;
+
+    $response.replies = $response.replies || [];
+    
+    $response.replies.push({
+        type: "text",
+        text: text,
+    })
+}
+
+function loadWeatherData(url, params) {
+    return getRequest(url, params);
+}
+
+
+function getPosition() {
+    var url = "http://api.openweathermap.org/geo/1.0/direct";
+    var city = $jsapi.context().request.query;
+    
+    var params = {
+        q: city,
+        appid: getAppId(),
+    }
+    
+    var position = getRequest(url, params);
+  
+    return {
+        lat: position.data[0].lon,
+        lon: position.data[0].lat,
+    };
+}
+
+function getRequest(url, params) {
+    return $http.query(mountParametersToQueryString(url, params));
+}
+
+function mountParametersToQueryString(url, paramsObj) {
+    var counter = 0;
+    var finalString = url;
+    
+    for (var field in paramsObj) {
+        if (!counter) {
+            finalString += '?' + field + '=';
+        } else {
+            finalString += '&'+ field + '=';
+        }
+        
+        finalString += paramsObj[field];
+        
+        counter++;
+    }
+  
+    return finalString;
+}
+
+function getAppId() {
+    return "6838f29871380f5b96e70075eb3f5613";
+}
